1cf9947b3c25c4243b601e7b65cfbbb0ec2295fe
Naitik Dabkara <naitik1dabkara08557@gmail.com>
2026-09-18 11:16:13 +0530

    Update answer4.c

--- lab04/error-detection/question4/answer4.c
+++ lab04/error-detection/question4/answer4.c
@@ -6,11 +6,11 @@ int main()
     printf("%d", 1number);
     return 0;
 }
+//correct code
 #include <stdio.h>
-
 int main()
 {
-    int 1number = 10;
-    printf("%d", 1number);
+    int number = 10;
+    printf("%d", number);
     return 0;
 }
