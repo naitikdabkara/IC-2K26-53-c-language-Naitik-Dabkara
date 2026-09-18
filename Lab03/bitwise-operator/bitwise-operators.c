be2fbd99664c9a40552ab1b66c7edfab144ba1eb
Naitik Dabkara <naitik1dabkara08557@gmail.com>
2026-09-18 10:20:42 +0530

    Create bitwise-operators.c

---
+++ Lab03/bitwise-operator/bitwise-operators.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int a = 5;
+    int b = 3;
+    printf("%d\n", a & b);  
+    printf("%d\n", a | b);  
+    printf("%d\n", a ^ b);
+    printf("%d\n", a << 1); 
+    printf("%d\n", a >> 1); 
+    return 0;
+}
