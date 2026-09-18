21f0ef8ef9f145bfaaaca483833bfb0f12bdf763
Naitik Dabkara <naitik1dabkara08557@gmail.com>
2026-09-18 11:10:54 +0530

    Create answer4.c

---
+++ lab04/error-detection/question4/answer4.c
@@ -0,0 +1,16 @@
+//wrong code
+#include <stdio.h>
+int main()
+{
+    int 1number = 10;
+    printf("%d", 1number);
+    return 0;
+}
+#include <stdio.h>
+
+int main()
+{
+    int 1number = 10;
+    printf("%d", 1number);
+    return 0;
+}
