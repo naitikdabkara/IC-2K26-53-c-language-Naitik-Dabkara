875f1ad11c02cd29f36d8868aa29c9e0bc49b51e
Naitik Dabkara <naitik1dabkara08557@gmail.com>
2026-09-18 11:04:43 +0530

    Create answer2.c

---
+++ lab04/error-detection/question2/answer2.c
@@ -0,0 +1,16 @@
+//wrong code
+#include <stdio.h>
+int main()
+{
+    int a = 10;
+    printf("%d", a);
+    return 0
+}
+//correct code
+#include <stdio.h>
+int main()
+{
+    int a = 10;
+    printf("%d", a);
+    return 0;
+}
