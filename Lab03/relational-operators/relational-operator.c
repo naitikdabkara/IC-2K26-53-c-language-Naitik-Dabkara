3373f087659674b1e45f7a84cff48fdb2d513e71
Naitik Dabkara <naitik1dabkara08557@gmail.com>
2026-09-18 10:23:38 +0530

    Create relational-operator.c

---
+++ Lab03/relational-operators/relational-operator.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int a = 10;
+    int b = 20;
+    printf("%d\n", a == b); 
+    printf("%d\n", a != b); 
+    printf("%d\n", a > b);  
+    printf("%d\n", a < b);  
+    printf("%d\n", a >= 10);
+    printf("%d\n", a <= b); 
+}
