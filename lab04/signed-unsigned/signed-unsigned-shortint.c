00e9d938d80edbbbce84541c71103549ffb18c50
Naitik Dabkara <naitik1dabkara08557@gmail.com>
2026-09-18 10:58:14 +0530

    Create signed-unsigned-shortint.c

---
+++ lab04/signed-unsigned/signed-unsigned-shortint.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    signed short int a = -32000; 
+    unsigned short int b = 60000;
+    printf("%d\n", a); 
+    printf("%u\n", b); 
+    signed short int c = 40000; 
+    printf("%d\n", c);
+    return 0;
+}
